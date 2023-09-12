67f1a7829fc1c23e7e7a4dad3def25a189757c97
William Touboul <Touboul.william@gmail.com>
2023-09-12 15:48:52 +0200

    lenis

--- asset/js/main.js
+++ asset/js/main.js
@@ -1,19 +1,19 @@
 let nav_state = false;
-let nav = document.querySelector('nav');
-let innernav = document.querySelector('.open_nav');
-let text = document.querySelector('#menu_instruction');
+let nav = document.querySelector("nav");
+let innernav = document.querySelector(".open_nav");
+let text = document.querySelector("#menu_instruction");
 function toggleNav() {
-    if (nav_state == false) {
-        innernav.classList.add('open_nav_is_open');
-        text.innerHTML = 'Close';
-        nav_state = true;
-        return nav_state;
-    } else {
-        innernav.classList.remove('open_nav_is_open');
-        text.innerHTML = 'Menu';
-        nav_state = false;
-        return nav_state;
-    }
+   if (nav_state == false) {
+      innernav.classList.add("open_nav_is_open");
+      text.innerHTML = "Close";
+      nav_state = true;
+      return nav_state;
+   } else {
+      innernav.classList.remove("open_nav_is_open");
+      text.innerHTML = "Menu";
+      nav_state = false;
+      return nav_state;
+   }
 }
 
-nav.addEventListener('click', toggleNav);
+nav.addEventListener("click", toggleNav);
