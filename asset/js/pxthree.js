b0cdff791a29789f120a74e7d46fa0ea9eb6fed9
William Touboul <Touboul.william@gmail.com>
2023-09-13 23:50:58 +0200

    doijd

--- asset/js/pxthree.js
+++ asset/js/pxthree.js
@@ -49,45 +49,45 @@ const fragmentShader = `
 `;
 
 function initializeScene(texture) {
-   //   scene creation
-   scene = new THREE.Scene();
+  //   scene creation
+  scene = new THREE.Scene();
 
-   // camera setup
-   camera = new THREE.PerspectiveCamera(
-      80,
-      imageElement.offsetWidth / imageElement.offsetHeight,
-      0.01,
-      10
-   );
-   camera.position.z = 1;
+  // camera setup
+  camera = new THREE.PerspectiveCamera(
+    80,
+    imageElement.offsetWidth / imageElement.offsetHeight,
+    0.01,
+    10
+  );
+  camera.position.z = 1;
 
-   //   uniforms
-   let shaderUniforms = {
-      u_mouse: { type: "v2", value: new THREE.Vector2() },
-      u_prevMouse: { type: "v2", value: new THREE.Vector2() },
-      u_aberrationIntensity: { type: "f", value: 0.0 },
-      u_texture: { type: "t", value: texture },
-   };
+  //   uniforms
+  let shaderUniforms = {
+    u_mouse: { type: "v2", value: new THREE.Vector2() },
+    u_prevMouse: { type: "v2", value: new THREE.Vector2() },
+    u_aberrationIntensity: { type: "f", value: 0.0 },
+    u_texture: { type: "t", value: texture }
+  };
 
-   //   creating a plane mesh with materials
-   planeMesh = new THREE.Mesh(
-      new THREE.PlaneGeometry(2, 2),
-      new THREE.ShaderMaterial({
-         uniforms: shaderUniforms,
-         vertexShader,
-         fragmentShader,
-      })
-   );
+  //   creating a plane mesh with materials
+  planeMesh = new THREE.Mesh(
+    new THREE.PlaneGeometry(2, 2),
+    new THREE.ShaderMaterial({
+      uniforms: shaderUniforms,
+      vertexShader,
+      fragmentShader
+    })
+  );
 
-   //   add mesh to scene
-   scene.add(planeMesh);
+  //   add mesh to scene
+  scene.add(planeMesh);
 
-   //   render
-   renderer = new THREE.WebGLRenderer();
-   renderer.setSize(imageElement.offsetWidth, imageElement.offsetHeight);
+  //   render
+  renderer = new THREE.WebGLRenderer();
+  renderer.setSize(imageElement.offsetWidth, imageElement.offsetHeight);
 
-   //   create a canvas
-   imageContainer.appendChild(renderer.domElement);
+  //   create a canvas
+  imageContainer.appendChild(renderer.domElement);
 }
 
 // use the existing image from html in the canvas
@@ -96,27 +96,26 @@ initializeScene(new THREE.TextureLoader().load(imageElement.src));
 animateScene();
 
 function animateScene() {
-   requestAnimationFrame(animateScene);
+  requestAnimationFrame(animateScene);
 
-   mousePosition.x += (targetMousePosition.x - mousePosition.x) * easeFactor;
-   mousePosition.y += (targetMousePosition.y - mousePosition.y) * easeFactor;
+  mousePosition.x += (targetMousePosition.x - mousePosition.x) * easeFactor;
+  mousePosition.y += (targetMousePosition.y - mousePosition.y) * easeFactor;
 
-   planeMesh.material.uniforms.u_mouse.value.set(
-      mousePosition.x,
-      1.0 - mousePosition.y
-   );
+  planeMesh.material.uniforms.u_mouse.value.set(
+    mousePosition.x,
+    1.0 - mousePosition.y
+  );
 
-   planeMesh.material.uniforms.u_prevMouse.value.set(
-      prevPosition.x,
-      1.0 - prevPosition.y
-   );
+  planeMesh.material.uniforms.u_prevMouse.value.set(
+    prevPosition.x,
+    1.0 - prevPosition.y
+  );
 
-   aberrationIntensity = Math.max(0.0, aberrationIntensity - 0.05);
+  aberrationIntensity = Math.max(0.0, aberrationIntensity - 0.05);
 
-   planeMesh.material.uniforms.u_aberrationIntensity.value =
-      aberrationIntensity;
+  planeMesh.material.uniforms.u_aberrationIntensity.value = aberrationIntensity;
 
-   renderer.render(scene, camera);
+  renderer.render(scene, camera);
 }
 
 // event listeners
@@ -125,27 +124,25 @@ imageContainer.addEventListener("mouseenter", handleMouseEnter);
 imageContainer.addEventListener("mouseleave", handleMouseLeave);
 
 function handleMouseMove(event) {
-   easeFactor = 0.02;
-   let rect = imageContainer.getBoundingClientRect();
-   prevPosition = { ...targetMousePosition };
+  easeFactor = 0.02;
+  let rect = imageContainer.getBoundingClientRect();
+  prevPosition = { ...targetMousePosition };
 
-   targetMousePosition.x = (event.clientX - rect.left) / rect.width;
-   targetMousePosition.y = (event.clientY - rect.top) / rect.height;
+  targetMousePosition.x = (event.clientX - rect.left) / rect.width;
+  targetMousePosition.y = (event.clientY - rect.top) / rect.height;
 
-   aberrationIntensity = 1;
+  aberrationIntensity = 1;
 }
 
 function handleMouseEnter(event) {
-   easeFactor = 0.02;
-   let rect = imageContainer.getBoundingClientRect();
+  easeFactor = 0.02;
+  let rect = imageContainer.getBoundingClientRect();
 
-   mousePosition.x = targetMousePosition.x =
-      (event.clientX - rect.left) / rect.width;
-   mousePosition.y = targetMousePosition.y =
-      (event.clientY - rect.top) / rect.height;
+  mousePosition.x = targetMousePosition.x = (event.clientX - rect.left) / rect.width;
+  mousePosition.y = targetMousePosition.y = (event.clientY - rect.top) / rect.height;
 }
 
 function handleMouseLeave() {
-   easeFactor = 0.05;
-   targetMousePosition = { ...prevPosition };
+  easeFactor = 0.05;
+  targetMousePosition = { ...prevPosition };
 }
